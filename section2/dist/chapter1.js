70a8f98df771b7ad7771cd7d20bda4a3ccca0329
DESKTOP-FIDOEO4\user <jungheon21@naver.com>
2025-05-08 01:22:38 +0900

    배열과 튜플

---
+++ section2/dist/chapter1.js
@@ -0,0 +1,24 @@
+//number
+let num1 = 123;
+let num2 = -123;
+let num3 = 0.123;
+let num4 = -0.123;
+let num5 = Infinity;
+let num6 = -Infinity;
+let num7 = NaN;
+num1.toFixed();
+// string
+let str1 = "hello";
+let str2 = `hello`;
+let str4 = `hello ${num1}`;
+//boolean
+let bool1 = true;
+// null
+let null1 = null;
+// undefined
+let unde1 = undefined;
+let numA = null; //strictNullChecks : false 일때
+// 리터럴 타입
+// 리터럴 -> 값
+let strA = "hello";
+export {};
